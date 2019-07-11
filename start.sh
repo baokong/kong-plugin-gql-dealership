f19dc964460720af1fab5cd012ab24c98c15dbdb
Bao Mai <bao.mai@konghq.com>
2019-07-11 16:36:39 -0700

    Start update

--- start.sh
+++ start.sh
@@ -7,10 +7,16 @@ git clone https://github.com/baokong/kong-plugin-gql-dealership.git
 apk add httpie
 http post :8001/services/ name=gql-server url=http://host.docker.internal:4000
 http post :8001/services/gql-server/routes hosts:='["dealership.com"]'
-export KONG_PLUGINS=bundled,gql-dealership
-kong reload
+
 cd /kong-plugin-gql-dealership
 luarocks make
 cd /
+
+export KONG_PLUGINS=bundled,gql-dealership
+kong reload
+
+http post :8001/services/gql-server/plugins/ name=gql-dealership
+kong prepare
+kong reload
 
 
